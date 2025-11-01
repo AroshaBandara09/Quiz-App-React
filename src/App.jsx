c8cbcf5072e34d449a7aa14fc0bf58c611f610dc
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-02 00:15:50 +0530

    remove the register page reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
