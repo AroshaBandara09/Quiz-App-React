bda72027519c79a85a8f430bcbbd8c3f6cf4e75e
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-01 23:55:43 +0530

    remove the dashboard route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
-import QuizDashboard from "./pages/quiz-dashboard/QuizDashboard";
 import Register from "./pages/register/Register";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<QuizDashboard />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </>
